46c5f297f83cd3accd3d91ddb45a6a1b103a4657
Mathew Brown <mathew.brown@hakai.org>
2022-01-27 12:28:07 -0800

    try not using web workers

--- src/for.js
+++ src/for.js
@@ -6,18 +6,12 @@ const loader = new THREE.ImageBitmapLoader().setOptions( { imageOrientation: 'fl
 onmessage = function (fileToLoad) {
   // console.log(fileToLoad)
   loader.load(fileToLoad.data, function (imageBitmap){
-    const texture = new THREE.CanvasTexture( imageBitmap );
+    // const texture = new THREE.CanvasTexture( imageBitmap );
     // console.log(imageBitmap)
     postMessage(imageBitmap)
   }, undefined, function (e){
     console.error(e);
-    
-
   }) 
-  // => {
-  //   // console.log(imageBitmap)
-    
-   
-  // })
+
 
 }
